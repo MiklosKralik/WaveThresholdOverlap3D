d0b099f3c7e0b2e80306668b82e5e26f5cdf8ff0
Miklos Kralik <miklos.a.kralik@gmail.com>
2023-02-04 16:56:25 +0100

    solver instable but can produce basic buggy outputs

--- wfc/wfc_tiler.py
+++ wfc/wfc_tiler.py
@@ -12,14 +12,22 @@ class TileDataset:
         self.bitcube = bitcube
         self.N = N
         self.O = N-1
+        self.form = form
+        self.wrap_pad = wrap_pad
+        
+        # extract all overlapping cubes
         bitcube_pad, cube_master = ut.extract_cubes(bitcube, form, N=N, wrap_pad=wrap_pad)
         cube_master = cube_master
         print(f'cube_master dtype {cube_master.dtype}')
+        
+        # if wrap_pad, remove padding of np nans
+
+        # get all unique cubes
         self.cubes, self.index, self.counts = np.unique(cube_master ,return_counts=True ,return_index=True, axis=0)
-        self.pad_shape = bitcube_pad.shape
         print(f'unique cubes: {len(self.cubes)}')
+
+        # calculate constraints
         self.constraints = ut.compare_cubes(self.cubes, form, N=N)
-        print(self.constraints['left'].shape)
 
     def __len__(self):
         return len(self.cubes)
